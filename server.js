5eb2709f7550d1c99d2c0346e105384566efc96e
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-04-27 21:05:41 -0400

    it works

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 var team = [];
-
+var teamNameEl = "";
 const initPrompt = () =>
   inquirer
     .prompt([
@@ -23,7 +23,7 @@ const initPrompt = () =>
     ])
     .then(data => {
       const teamName = data.teamName;
-      team.push(teamName);
+      teamNameEl = teamName;
       createManager();
     });
 
@@ -174,27 +174,96 @@ function createIntern(obj) {
 }
 
 function generateTeam() {
-  // build a function that returns a string `<div> ${OBJECT.name} </div> `
-  console.log(team);
-
-  // some functionality for deterimining what ROLE a given EMPLOYEE OBJECT we have
-  
-  
   // create a variable to hold each each STRING from functions
   let team_cards = [];
+  let card_string = "";
+  // build a function that returns a string `<div> ${OBJECT.name} </div> `
+  const cardWriter = team_cards => {
+    card_string = JSON.stringify(team_cards);
+    team_cards.join(" ");
+  };
+  // some functionality for deterimining what ROLE a given EMPLOYEE OBJECT we have
+  console.log(team);
+  let newCard;
+  for (var i = 0; i < team.length; i++) {
+    switch (team[i].role) {
+      case "Manager":
+        newCard = managerCard(team[i]);
+        team_cards.push(newCard);
+        break;
+      case "Intern":
+        newCard = internCard(team[i]);
+        team_cards.push(newCard);
+        break;
+      case "Engineer":
+        newCard = engineerCard(team[i]);
+        team_cards.push(newCard);
+        break;
+      default:
+        console.log("role call failed");
+    }
+    cardWriter(team_cards);
+
+    // Call our next loop function
+  }
+
+  console.log(team_cards);
 
   // Create A Card
   function managerCard(manager) {
-      return `
-        <div> 
-          <h3>Name: ${manager.getName()}</h3>
-          <p>${manager.getRole()}</p> 
-        </div>
-      `
+    return `
+<div class="card" style="width:20%">
+  <div class="card-header">
+    <h3>Name: ${manager.name}</h3>
+    <h3><i class="fas fa-mug-hot"></i>${manager.role}</h3>
+  </div>
+  <div class="card-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id:${manager.id}</li>
+      <li class="list-group-item">Email:<a href="mailto: ${manager.email}">${manager.email}</a></li>
+      <li class="list-group-item">Office Number:${manager.office} </li>
+    </ul>
+  </div>
+</div>
+      `;
+  }
+  function engineerCard(engineer) {
+    return `
+<div class="card" style="width:20%">
+  <div class="card-header">
+    <h3>Name: ${engineer.name}</h3>
+    <h3><i class="fas fa-glasses"></i>${engineer.role}</h3>
+  </div>
+  <div class="card-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id:${engineer.id}</li>
+      <li class="list-group-item">Email:<a href="mailto: ${engineer.email}">${engineer.email}</a></li>
+      <li class="list-group-item">Github:<a href="https://github.com/${engineer.gitHub}" target="_blank">${engineer.gitHub}</a></li>
+    </ul>
+  </div>
+</div>
+      `;
+  }
+  function internCard(intern) {
+    return `
+<div class="card" style="width:20%">
+  <div class="card-header">
+    <h3>Name: ${intern.name}</h3>
+    <h3><i class="fas fa-school"></i>${intern.role}</h3>
+  </div>
+  <div class="card-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id:${intern.id}</li>
+      <li class="list-group-item">Email:<a href="mailto: ${intern.email}">${intern.email}</a></li>
+      <li class="list-group-item">School:${intern.school}</li>
+    </ul>
+  </div>
+</div>
+      `;
   }
 
   // Create our Base HTML file
-  function pageBuilder()  {
+  const pageBuilder = card_string => {
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -202,18 +271,39 @@ function generateTeam() {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
         <title>Document</title>
     </head>
     <body>
-      ${team_cards} 
+    <header style="background-color:red" width="100%" height="20vh">
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-12 jumbotron mb-3 team-heading">
+            <h1 class="text-center">${teamNameEl}</h1>
+        </div>
+    </div>
+</div>
+</header>
+    <div class="container" style="padding: 20px;">
+        <div class="row team-area col-12 d-flex justify-content-center">
+          ${team_cards} 
+        </div>
+     </div>
+
     </body>
     </html>
-    `
+    `;
+  };
+
+  try {
+    const html = pageBuilder(team_cards);
+    fs.writeFileSync("teamProfile.html", html);
+    console.log("Successfully wrote to teamProfile.html");
+  } catch (error) {
+    console.log(error);
   }
-
-
-
 }
 
 initPrompt();
